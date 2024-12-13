ad6f53ce54c947468e370a0a114c99c3cdcd41cd
akcode7 <ashishgotapogo10@gmail.com>
2024-12-14 00:56:53 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{php,html,js},",
+    "./pay/*.{php,html,js},",
+    "./recharge/*.{php,html,js},",
+    "./*.{html,php}",
+    "./src/css/**/*.css",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
